575fcb096e8ae37f68e8662b8d5447c3e4fa199b
Dahrio-Francois <dahrio.francois@mths.ca>
2022-04-14 18:38:53 +0000

    added looping.cpp

---
+++ looping.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) 2022 St. Mother Teresa HS All rights reserved.
+//
+// Created by: Dahrio Francois
+// Created on: April 2022
+// This program uses a while loop to do math
+
+#include <iostream>
+
+int main() {
+    // this function uses a while loop
+    std::string integerAsString;
+    int integerAsNumber;
+    int loopCounter = 0;
+    int calculation = 0;
+
+    // input
+    try {
+        std::cout << "Enter in an integer: ";
+        std::cin >> integerAsString;
+
+        // process & output
+        integerAsNumber = std::stoi(integerAsString);
+        while (loopCounter <= integerAsNumber) {
+        calculation = calculation + loopCounter;
+        loopCounter = loopCounter + 1;
+        } std::cout << "\nThe sum of those number is " <<
+        calculation << std::endl;
+    } catch (std::invalid_argument) {
+        std::cout << "\nThat was not a valid integer.";
+}
+}
